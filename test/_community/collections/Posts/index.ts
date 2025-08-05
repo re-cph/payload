a47477b4b2407415708eeb860205d02102086d8f
Mikkel Wied Frederiksen <mikkel@wied.cc>
2025-08-05 16:23:23 +0200

    Tabs issues commit

--- test/_community/collections/Posts/index.ts
+++ test/_community/collections/Posts/index.ts
@@ -1,6 +1,8 @@
 import type { CollectionConfig } from 'payload'
 
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { textColumn } from '../../fields/TextColumn/index.js'
+import { TFunction } from '@payloadcms/translations'
 
 export const postsSlug = 'posts'
 
@@ -15,11 +17,42 @@ export const PostsCollection: CollectionConfig = {
       type: 'text',
     },
     {
-      name: 'content',
-      type: 'richText',
-      editor: lexicalEditor({
-        features: ({ defaultFeatures }) => [...defaultFeatures],
-      }),
+      type: 'tabs',
+      tabs: [
+        {
+          label: ({ t }: { t: TFunction }) => t('fields:blocks'),
+          fields: [
+            {
+              name: 'blocks',
+              blocks: [textColumn],
+              localized: true,
+              required: true,
+              type: 'blocks',
+            },
+          ],
+        },
+
+        {
+          label: 'content',
+          fields: [
+            {
+              name: 'content',
+              type: 'richText',
+              editor: lexicalEditor({
+                features: ({ defaultFeatures }) => [...defaultFeatures],
+              }),
+            },
+          ],
+        },
+      ],
     },
   ],
+  versions: {
+    drafts: {
+      autosave: {
+        interval: 500, // We set this interval for optimal live preview
+      },
+    },
+    maxPerDoc: 50,
+  },
 }
